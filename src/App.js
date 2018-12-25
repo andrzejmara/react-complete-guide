f7dd92f0953dacdbd30d8278c13b6555c8ffb145
Andrew_Mara <andrzejmara@gmail.com>
2018-12-25 12:54:33 +0100

    Understanding the fundamentals of React

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [{
+      name: 'Andrzej', age:24},{name: 'Wiktoria',age: 22}, {name:'Mateusz',age:25}
+    ]
+  }
+switchNameHandler = (newName) => {
+  this.setState({persons: [{
+    name: newName, age:24},{name: 'Wiktoria',age: 28}, {name:'Mati',age:25}
+  ]
+})
+}
+
+nameChangedHandler = (event) => {
+  this.setState({persons: [{
+    name: 'Andrzej', age:24},{name: 'Wiktoria',age: 28}, {name:event.target.value,age:25}
+  ]
+})
+}
+
   render() {
+      const style={
+        backgroundColor:"white",
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+      };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Siemanko, jestem Andrzej</h1>
+      <p>I love you</p>
+      <button style={style} onClick={() => this.switchNameHandler('Andrzejek!')}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person click={this.switchNameHandler.bind(this,'Szemkel')} name={this.state.persons[1].name} age={this.state.persons[1].age}>Lubię jeść babeczki!</Person>
+      <Person changed={this.nameChangedHandler} name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
+    // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi! I\'m Andrzej!!!'));
   }
 }
 
