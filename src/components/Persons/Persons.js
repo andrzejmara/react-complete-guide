dca21669dd7192928a6245780459a5691cb45f3e
Andrzej Mara <andrzej.mara@remotemyapp.com>
2019-04-12 16:54:50 +0200

    Learn about Component Creation Lifecycle

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+import Person from "./Person/Person";
+
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log("[Persons.js] getDerivedStateFromProps");
+  //   return true;
+  // }
+
+  // componentWillReceiveProps(props) {
+  //   console.log("[Person.js] componentWillReceiveProps");
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] shouldComponentUpdate");
+    return true;
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Person.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Person.js] componentDidUpdate");
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log("[Persons.js] rendering...");
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={event => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
+}
+
+export default Persons;
